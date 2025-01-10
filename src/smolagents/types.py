6743d01ed5c8bed234f42ea313fa6ee97ea8e731
Aymeric <aymeric.roucher@gmail.com>
2025-01-10 21:45:52 +0100

    Formatting

--- src/smolagents/types.py
+++ src/smolagents/types.py
@@ -277,7 +277,9 @@ def handle_agent_output_types(output, output_type=None):
         # If the class does not have defined output, then we map according to the type
         for _k, _v in INSTANCE_TYPE_MAPPING.items():
             if isinstance(output, _k):
-                if not isinstance(output, object): # avoid converting to audio if torch is not installed
+                if not isinstance(
+                    output, object
+                ):  # avoid converting to audio if torch is not installed
                     return _v(output)
         return output
 

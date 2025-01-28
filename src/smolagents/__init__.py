022947a2a587483ac897f403f52a1e0a50f53667
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2025-01-28 12:06:07 +0100

    Bump dev version: v1.7.0.dev0 (#390)

--- src/smolagents/__init__.py
+++ src/smolagents/__init__.py
@@ -14,7 +14,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-__version__ = "1.6.0.dev"
+__version__ = "1.7.0.dev0"
 
 from .agents import *
 from .default_tools import *

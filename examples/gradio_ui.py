82e647abb03781358925e0c64635e288bdc7c77b
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2025-02-27 11:08:57 +0100

    Fix CI quality in examples/gradio_ui (#822)

--- examples/gradio_ui.py
+++ examples/gradio_ui.py
@@ -7,7 +7,7 @@ agent = CodeAgent(
     max_steps=4,
     verbosity_level=1,
     name="example_agent",
-    description="This is an example agent that has no tools and uses only code."
+    description="This is an example agent that has no tools and uses only code.",
 )
 
 GradioUI(agent, file_upload_folder="./data").launch()
